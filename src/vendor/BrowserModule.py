89d33898723b43dd041d7ecb80ec5373593d9d24
Fernando Jiménez <fertex_10mil@hotmail.com>
2020-11-23 12:07:17 -0600

    App relative dependencies

--- src/vendor/BrowserModule.py
+++ src/vendor/BrowserModule.py
@@ -5,7 +5,7 @@ from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, UnexpectedAlertPresentException
 
-from libs.db_manager import DbDriver
+from .libs.db_manager import DbDriver
 
 
 class WebActions:
