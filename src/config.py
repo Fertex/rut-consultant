91e1cb41628097571998a7b290c0d05bf161a42d
Fernando Jiménez <fertex_10mil@hotmail.com>
2021-07-13 09:02:54 -0500

    Judicial orders added

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ APP_NAME = 'RUT API'
 APP_SECRET = '_n2neW.aacB2^(X'
 TTE = 3600
 USERS = ['gNS8JHUx0X']
-SERVING_PORT = 4000
+SERVING_PORT = 5000
+SERVING_HOST = None
 
-DRIVER_PATH = r''
+DRIVER_PATH = r'E:\Program Files\chromedriver\chromedriver.exe'
