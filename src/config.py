4179cc7fc17a7f2e2049a8410f903fdf2d537783
Fertex <48325126+Fertex@users.noreply.github.com>
2021-07-26 17:33:49 -0500

    Updating driver path for deployment

--- src/config.py
+++ src/config.py
@@ -5,6 +5,6 @@ USERS = ['gNS8JHUx0X']
 SERVING_PORT = 4000
 SERVING_HOST = "0.0.0.0"
 
-DRIVER_PATH = r'E:\Program Files\chromedriver\chromedriver.exe'
+DRIVER_PATH = r''
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' \
              '(KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'
